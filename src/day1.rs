c99b68bba15f6c2e0b94386236713059225ea60c
TheRoniOne <ronidani99@hotmail.com>
2022-06-03 22:00:34 -0500

    Move day 1 functions to new module

---
+++ src/day1.rs
@@ -0,0 +1,89 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+pub fn day1_p1() -> i32 {
+    let file = File::open("inputs/day1_p1.txt").expect("File not found");
+    let mut measurements = BufReader::new(file).lines();
+
+    let mut prev = measurements
+        .next()
+        .unwrap()
+        .unwrap()
+        .parse::<i32>()
+        .unwrap();
+
+    let mut count = 0;
+
+    for measure in measurements {
+        if let Ok(measure) = measure {
+            let measure_int = measure.parse::<i32>().unwrap();
+            if is_deeper(&prev, &measure_int) {
+                count += 1;
+            }
+            prev = measure_int
+        }
+    }
+
+    return count;
+}
+
+pub fn day1_p2() -> i32 {
+    let file = File::open("inputs/day1_p1.txt").expect("File not found");
+    let mut measurements = BufReader::new(file).lines();
+
+    let mut first = measurements
+        .next()
+        .unwrap()
+        .unwrap()
+        .parse::<i32>()
+        .unwrap();
+
+    let mut second = measurements
+        .next()
+        .unwrap()
+        .unwrap()
+        .parse::<i32>()
+        .unwrap();
+
+    let mut third = measurements
+        .next()
+        .unwrap()
+        .unwrap()
+        .parse::<i32>()
+        .unwrap();
+
+    let mut prev_sum = first + second + third;
+
+    let mut count = 0;
+
+    for measure in measurements {
+        if let Ok(new_value) = measure {
+            first = second;
+            second = third;
+            third = new_value.parse::<i32>().unwrap();
+            let new_sum = first + second + third;
+
+            if is_deeper(&prev_sum, &new_sum) {
+                count += 1;
+            }
+            prev_sum = new_sum;
+        }
+    }
+
+    return count;
+}
+
+fn is_deeper(prev: &i32, measure: &i32) -> bool {
+    if prev < measure {
+        println!("{measure} (increased)");
+        return true;
+    } else if prev == measure {
+        println!("{measure} (equal)");
+        return false;
+    } else {
+        println!("{measure} (decreased)");
+        return false;
+    }
+}
