d72be84d93b4eafccb216e972b45970c6fc6c5c2
TheRoniOne <ronidani99@hotmail.com>
2022-06-04 15:50:35 -0500

    Add solution for day2_p2

--- src/day2.rs
+++ src/day2.rs
@@ -31,3 +31,36 @@ pub fn day2_p1() -> i32 {
 
     return position.x * position.y;
 }
+
+struct Position2 {
+    x: i32,
+    y: i32,
+    aim: i32,
+}
+
+impl Position2 {
+    fn _move(&mut self, direction: &str, amount: i32) {
+        match direction {
+            "forward" => {
+                self.x += amount;
+                self.y += self.aim * amount
+            }
+            "up" => self.aim -= amount,
+            "down" => self.aim += amount,
+            _ => (),
+        }
+    }
+}
+
+pub fn day2_p2() -> i32 {
+    let file = File::open("inputs/day2_p1.txt").expect("File not found");
+    let commands = BufReader::new(file).lines();
+
+    let mut position: Position2 = Position2 { x: 0, y: 0, aim: 0 };
+    for command in commands.flatten() {
+        let v: Vec<&str> = command.split(' ').collect();
+        position._move(v[0], v[1].parse::<i32>().unwrap());
+    }
+
+    return position.x * position.y;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 mod day1;
 mod day2;
 
-use crate::day2::day2_p1;
+use crate::day2::day2_p2;
 
 fn main() {
-    let result = day2_p1();
+    let result = day2_p2();
     println!("{result} is the final result");
 }
